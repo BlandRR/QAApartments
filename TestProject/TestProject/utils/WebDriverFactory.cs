21379b8f47302c8c4f402047718a415387837c4b
Ashley <ashley.beeson@qa.com>
2018-01-22 15:02:40 +0000

    Created a util for webdriver

---
+++ TestProject/TestProject/utils/WebDriverFactory.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.utils
+{
+    class WebDriverFactory
+    {
+
+        public static IWebDriver CreateDriver(Browser browser)
+        {
+            switch (browser)
+            {
+                default: throw new NotImplementedException("Browser: " + browser.ToString() + " Support not yet implemented");
+                case Browser.Chrome: return new ChromeDriver();
+                case Browser.Edge: return new EdgeDriver();
+            }
+        }
+
+
+        public enum Browser
+        {
+            Chrome,Edge
+        }
+
+    }
+}
