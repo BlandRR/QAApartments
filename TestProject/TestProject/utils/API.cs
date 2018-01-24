1dfd40202233c9aa88e64a38d8a91f11f62133fe
SamCrossley <samuel.r.crossley@outlook.com>
2018-01-24 07:11:17 +0000

    api requester and spreadsheet reader

---
+++ TestProject/TestProject/utils/API.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace TestProject
+{
+    class API
+    {
+        SpreadSheetReader reader;
+        public IRestResponse ApiCall(String method, String hostName, int port, String url, String jsonBody)
+        {
+            var client = new RestClient(hostName + port + url);
+            var request = new RestRequest(RestType(method));
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("undefined", jsonBody, ParameterType.RequestBody);
+            return client.Execute(request);
+
+            
+
+        }
+
+        private Method RestType(String method)
+        {
+            switch (method)
+            {
+                default: return Method.GET;
+                case "GET": return Method.GET;
+                case "POST": return Method.POST;
+                case "PUT": return Method.PUT;
+                case "DELETE": return Method.DELETE;
+                
+               
+
+
+            }
+
+            
+                    
+        }
+    }
+}
